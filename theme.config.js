9ad3836073640d8ad90764441472a1b1ef9bdfea
Robert Crocker <robert@vizsimply.com>
2022-01-05 01:53:49 -0300

    In order to get syntax highlighting I literally had to fork the Nextra site and overwite what we had with the code from that repo.

--- theme.config.js
+++ theme.config.js
@@ -14,8 +14,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="D3 Reacts Docs" />
-      <meta name="og:description" content="D3 Reacts Docs" />
+      <meta name="description" content="Nextra: the Next.js site builder" />
+      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="nextra.vercel.app" />
+      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
