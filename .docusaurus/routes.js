a83dda3fd3954d041458c21125489762dfa19ac9
irbopia <travislockett1@gmail.com>
2022-01-06 12:47:35 -0500

    Boiler plate update

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -4,41 +4,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug','3d6'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config','914'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content','c28'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry','0da'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes','244'),
-    exact: true
-  },
-  {
     path: '/blog',
     component: ComponentCreator('/blog','520'),
     exact: true
@@ -95,7 +60,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','0c2'),
+    component: ComponentCreator('/docs','7ef'),
     routes: [
       {
         path: '/docs/Charts/area-chart',
@@ -105,7 +70,7 @@ export default [
       },
       {
         path: '/docs/Charts/bar-chart',
-        component: ComponentCreator('/docs/Charts/bar-chart','59b'),
+        component: ComponentCreator('/docs/Charts/bar-chart','e80'),
         exact: true,
         'sidebar': "tutorialSidebar"
       },
