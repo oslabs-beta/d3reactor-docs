829806edded7c87c2353b63ef68701694556f521
irbopia <travislockett1@gmail.com>
2022-01-29 13:51:30 -0500

    D3 reacts to d3reactor

--- .docusaurus/docusaurus.config.js
+++ .docusaurus/docusaurus.config.js
@@ -16,7 +16,7 @@ export default {
       "classic",
       {
         "docs": {
-          "sidebarPath": "/Volumes/Faero2TB/DocumentsEXT/Codesmith/projects/OSP/d3reacts-docs/sidebars.js",
+          "sidebarPath": "/Users/travislockett/Codesmith/OSP/d3reactor-docs/sidebars.js",
           "editUrl": "https://github.com/oslabs-beta/d3reacts-website"
         },
         "blog": {
@@ -24,7 +24,7 @@ export default {
           "editUrl": "https://github.com/oslabs-beta/d3reacts-website/"
         },
         "theme": {
-          "customCss": "/Volumes/Faero2TB/DocumentsEXT/Codesmith/projects/OSP/d3reacts-docs/src/css/custom.css",
+          "customCss": "/Users/travislockett/Codesmith/OSP/d3reactor-docs/src/css/custom.css",
           "themes": [
             "@docusaurus/theme-live-codeblock"
           ]
